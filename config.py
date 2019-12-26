8f0980b3ff247abd48270760808eb722e292977f
dimitramav <dmavrof@gmail.com>
2019-12-26 20:06:25 +0200

    config

---
+++ config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "4OlkTqJmjaldPXW3Aqe6RUTo0"
+consumer_secret = "Rujs81iiCgw1u0ZbBMZCQneImazJA8wnImqWbn5wGgpvgftKAg"
+access_key = "1119890635279015936-4euB1D3u1O9EKyI5kuPSfChTC8LoTQ"
+access_secret = "gsp51xMjCvBlSt7kB3s1eNxSfqhRqozYwefZaAW9V5msE"
