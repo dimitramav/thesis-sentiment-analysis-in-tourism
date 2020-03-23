cadb3341efb705749a26931d28310cb634aa2ca6
Dimitra Mavroforaki <dmavrof@gmail.com>
2020-03-23 10:42:17 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "4OlkTqJmjaldPXW3Aqe6RUTo0"
-consumer_secret = "Rujs81iiCgw1u0ZbBMZCQneImazJA8wnImqWbn5wGgpvgftKAg"
-access_key = "1119890635279015936-4euB1D3u1O9EKyI5kuPSfChTC8LoTQ"
-access_secret = "gsp51xMjCvBlSt7kB3s1eNxSfqhRqozYwefZaAW9V5msE"
+consumer_key = ""
+consumer_secret = ""
+access_key = "-"
+access_secret = ""
